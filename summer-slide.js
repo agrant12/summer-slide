f951a398b3aadb4592a151179bb586b5ee41c7ee
Alvin Grant <alving.nyc@gmail.com>
2013-05-14 10:24:14 -0400

    Add support for iPad, iPhone, and Android devices

--- summer-slide.js
+++ summer-slide.js
@@ -1,9 +1,11 @@
 (function($){
 	$.fn.hamburger = function(options){
+
 		var $button = $('.button');
 		var $tray = $('.tray');
 		var $dist = $tray.width();
 		var $page = $('.page');
+		var $myLocation = 0;
 		var defaultToggleState = true;
 
 		var settings = $.extend({
@@ -11,29 +13,74 @@
 			"width": 250
 		}, options);
 
-		$button.on('click', function(ev){
+		if (/(iPhone|iPad|iPod|Android)/i.test(navigator.userAgent)){
 
-			if (defaultToggeleState){
-				$tray.animate({
-					'margin-left': $dist + 'px'
-				});
+			$button.on('click', function(ev){
 
-				$page.animate({
-					'margin-left': $dist + 'px'
-				});
-				defaultToggleState = false;
-			} else {
-				$tray.animate({
-					'margin-left': 0
-				});
+				if (defaultToggeleState){
 
-				$page.animate({
-					'margin-left': 0
-				});
+					$myLocation = $(window).scrollTop();
 
-				defaultToggleState = true;
-			}
-			ev.preventDefault();
-		});
+					$tray.animate({
+						'margin-left': $dist + 'px'
+					});
+
+					$page.animate({
+						'margin-left': $dist + 'px',
+						'position': 'fixed',
+						'top': '-' + $myLocation + 'px'
+					});
+
+					defaultToggleState = false;
+
+				} else {
+
+					$(window).scrollTop($myLocation);
+
+					$tray.animate({
+						'margin-left': 0
+					});
+
+					$page.animate({
+						'margin-left' : 0
+					});
+
+					defaultToggleState = true;
+
+				}
+
+		} else {
+
+			$button.on('click', function(ev){
+
+				if (defaultToggeleState){
+					
+					$tray.animate({
+						'margin-left': $dist + 'px'
+					});
+
+					$page.animate({
+						'margin-left': $dist + 'px'
+					});
+
+					defaultToggleState = false;
+
+				} else {
+
+					$tray.animate({
+						'margin-left': 0
+					});
+
+					$page.animate({
+						'margin-left': 0
+					});
+
+					defaultToggleState = true;
+				}
+
+				ev.preventDefault();
+
+			});
+		}
 	};
 });
